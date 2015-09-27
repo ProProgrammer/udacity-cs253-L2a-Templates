92e66116c0ef11c888853c88ad14d0911e8a6237
Deep Sukhwani <deepsukhwani@gmail.com>
2015-09-27 23:29:31 +0530

    Initial Commit of L2A > Writing a Basic Form

---
+++ templates.py
@@ -0,0 +1,16 @@
+import os
+import webapp2
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.write(*a, **kw)
+
+
+class MainPage(Handler):
+	def get(self):
+		self.write("Hello Udacity!")
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+							],
+							debug=True)
