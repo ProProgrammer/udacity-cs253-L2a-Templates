bb526d76ab035481f8624892e0199c2eb2168c84
Deep Sukhwani <deepsukhwani@gmail.com>
2015-09-28 20:20:55 +0530

    L2a > Introducing Templates (Jinja2)

--- templates.py
+++ templates.py
@@ -1,14 +1,10 @@
 import os
+import jinja2
+from jinja2 import Environment
 import webapp2
 
-form_html = """
-<form>
-	<h2>Add a food</h2>
-	<input type='text' name='food'>
-	%s
-	<button>Add</button>
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 hidden_html = """
 <input type='hidden' name='food' value='%s'>
@@ -32,10 +28,37 @@ class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.write(*a, **kw)
 
+	def render_str(self, template, **params):
+		"""
+		'template' is the filename
+		**params > bunch of extra parameters
+		"""
+
+		# using the jinja environment that we created above
+		# calling get_template() function on it passing in the file name
+		# This causes jinja to load that file and create a jinja template object stored in t
+ 		t = jinja_env.get_template(template)
+
+ 		# This simply renders the template
+ 		return t.render(params)
+
+ 	def render(self, template, **kw):
+ 		"""
+ 		This calls render_str() that we defined above and wraps it into self.write() which actually sends it back to the browser 
+ 		"""
+
+ 		self.write(self.render_str(template, **kw))
+
+
+
 # We will prefix all the variables that are holding the output html with the word "output"
 
 class MainPage(Handler):
 	def get(self):
+		self.render("shopping_list.html")
+
+		# Commenting out other stuff (as below) for now
+		"""
 		output = form_html
 		output_hidden = ""
 
@@ -51,6 +74,7 @@ class MainPage(Handler):
 		
 		output = output % output_hidden
 		self.write(output)
+		"""
 
 app = webapp2.WSGIApplication([('/', MainPage),
 							],
